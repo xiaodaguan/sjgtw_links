222894287817bd527a2cdf67c0c8075e04ebfec0
guanxiaoda <xiaodaguan@gmail.com>
2016-03-24 15:24:31 +0800

    完善: 已经parse过的页面不再请求,断点爬取时需人工决定上次爬取过哪些url

--- sjgtw_links/spiders/linkSpider.py
+++ sjgtw_links/spiders/linkSpider.py
@@ -9,7 +9,7 @@ import codecs
 class linkSpider(scrapy.Spider):
     name = 'sjgtw_links'
     allow_domains = ["sjgtw.com"]
-    start_urls = ['http://www.sjgtw.com/goodsClass/goodsClassIndex?clickId=4522']
+    start_urls = ['http://www.sjgtw.com/goodsClass/goodsClassIndex?clickId=1']
     headers = {
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
         "Cookie": "clientId=38681443894000; clientId=38681446210000; JSESSIONID=372570A67852F5343A0E9C01BA4F5F6D; Hm_lvt_8908fa41684e227cc7f033849052cd8a=1458723279; Hm_lpvt_8908fa41684e227cc7f033849052cd8a=1458723806; CNZZDATA1256025023=1839444177-1458720018-%7C1458720018"
@@ -61,5 +61,9 @@ class linkSpider(scrapy.Spider):
             else:
                 print("url seen %s" % href)
             if href not in self.urls_crawled:  # 已经爬过的的页面不再爬取
+                print("crawling %s from %s " % (href, response.url))
                 yield scrapy.Request(href, callback=self.parse)
+            else:
+                print("url %s crawled before... pass" % href)
         self.urls_crawled.add(response.url)
+        print("url %s added to urls_crawled." % response.url)
