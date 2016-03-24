f9c32966daa3023e2ef1145b1e1d522a44f643a3
guanxiaoda <xiaodaguan@gmail.com>
2016-03-24 15:15:54 +0800

    完善: 已经parse过的页面不再请求

--- sjgtw_links/spiders/linkSpider.py
+++ sjgtw_links/spiders/linkSpider.py
@@ -9,7 +9,7 @@ import codecs
 class linkSpider(scrapy.Spider):
     name = 'sjgtw_links'
     allow_domains = ["sjgtw.com"]
-    start_urls = ['http://www.sjgtw.com/goodsClass/goodsClassIndex?clickId=1']
+    start_urls = ['http://www.sjgtw.com/goodsClass/goodsClassIndex?clickId=4522']
     headers = {
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
         "Cookie": "clientId=38681443894000; clientId=38681446210000; JSESSIONID=372570A67852F5343A0E9C01BA4F5F6D; Hm_lvt_8908fa41684e227cc7f033849052cd8a=1458723279; Hm_lpvt_8908fa41684e227cc7f033849052cd8a=1458723806; CNZZDATA1256025023=1839444177-1458720018-%7C1458720018"
@@ -20,6 +20,16 @@ class linkSpider(scrapy.Spider):
         self.file = codecs.open('links.txt', 'w')
         self.file.write("level\tname\tclickId\turl\tfrom_url\n")
         self.urls_seen = set()
+        self.urls_crawled = set()
+
+        crawled = open("crawled.txt", 'r')
+        while 1:
+            line = crawled.readline()
+            if not line:
+                break
+            self.urls_crawled.add(line)
+
+        print(">>>> %d crawled links loaded. " % len(self.urls_crawled))
 
     def parse(self, response):
         catalogys = response.xpath("//div[@class='row']/span[@class='catalogy_span']")
@@ -50,5 +60,6 @@ class linkSpider(scrapy.Spider):
                 self.urls_seen.add(item['url'])
             else:
                 print("url seen %s" % href)
-            yield scrapy.Request(href, callback=self.parse)  # def parse_list(self, response):
-            #     for category in response.xpath("//a[contains(@href,'goodsClass')]"):
+            if href not in self.urls_crawled:  # 已经爬过的的页面不再爬取
+                yield scrapy.Request(href, callback=self.parse)
+        self.urls_crawled.add(response.url)
